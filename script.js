93f3ca835e9d2818a50f13a576f41b07c1ecf2fe
yk-david <81497136+yk-david@users.noreply.github.com>
2024-05-06 13:47:56 +0000

    set up logic for random number

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 30;
+
+// document.querySelector('.guess').value = 21;
+// console.log(document.querySelector('.guess').innerHTML);
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  }
+});
