704531360cb029fd52853e0f84cc1e3c329cb1c9
yk-david <81497136+yk-david@users.noreply.github.com>
2024-05-08 13:28:23 +0000

    Refactoring for eliminating duplicate code

--- script.js
+++ script.js
@@ -24,22 +24,11 @@ document.querySelector('.check').addEventListener('click', function() {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-      
-  // When guess is too high
-  } else if (guess > secretNumber) {
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🥹 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  
-  // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
+      document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -47,7 +36,9 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.score').textContent = 0;
     }
     
-  }
+ 
+  }   
+  
 });
 
 
